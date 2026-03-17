044d7fe76f7872101e77b6cf7e269613aafd1926
Alexis Morel <22501205@me.uvsq.fr>
2026-03-17 14:53:27 +0100

    initialisation du projet

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.deuxieme_projet {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens com.example.deuxieme_projet to javafx.fxml;
+    exports com.example.deuxieme_projet;
+}
