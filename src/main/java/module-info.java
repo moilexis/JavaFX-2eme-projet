fc1af23943a849e5b91acc9e50f4e5ca1d397099
Alexis Morel <22501205@me.uvsq.fr>
2026-03-24 16:10:44 +0100

    début du 2.27 : label du mois

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module com.example.deuxieme_projet {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    //    requires com.example.deuxieme_projet;
+
 
     opens com.example.deuxieme_projet to javafx.fxml;
     exports com.example.deuxieme_projet;
--- src/main/java/vue/VBoxRoot.java
+++ src/main/java/vue/VBoxRoot.java
@@ -2,18 +2,27 @@ package vue;
 
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import modele.CalendrierDuMois;
+import modele.ConstantesCalendrier;
 import modele.DateCalendrier;
 
 public class VBoxRoot extends VBox {
 
     public VBoxRoot(int spacing) {
         super.setSpacing(spacing);
+        /*
         DateCalendrier dateDuJour = new DateCalendrier();
         Label aujourdhui = new Label(dateDuJour.toString());
         getChildren().add(aujourdhui);
         DateCalendrier dateDemain = dateDuJour.dateDuLendemain();
         Label demain = new Label(dateDemain.toString());
         getChildren().add(demain);
+        */
+        DateCalendrier dateDuJour = new DateCalendrier();
+        CalendrierDuMois ceMois = new CalendrierDuMois(dateDuJour.getMois(),dateDuJour.getAnnee());
+        Label etiCeMois = new Label(ConstantesCalendrier.Mois.values() [ceMois.getMois()-1]+ " " + ceMois.getAnnee());
+        getChildren().add(etiCeMois);
+
     }
 
 
