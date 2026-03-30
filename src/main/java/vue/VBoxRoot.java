5eb45242d52fcaf743a911ae12e6a146e10e205f
Alexis Morel <22501205@me.uvsq.fr>
2026-03-30 11:46:26 +0200

    implémentation de la stackPane

--- src/main/java/vue/VBoxRoot.java
+++ src/main/java/vue/VBoxRoot.java
@@ -4,6 +4,7 @@ package vue;
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import modele.CalendrierDuMois;
 import modele.ConstantesCalendrier;
@@ -15,27 +16,43 @@ public class VBoxRoot extends VBox {
 
     public VBoxRoot(int spacing) {
         super.setSpacing(spacing);
-
         DateCalendrier dateDuJour = new DateCalendrier();
         CalendrierDuMois ceMois = new CalendrierDuMois(dateDuJour.getMois(),dateDuJour.getAnnee());
-        Label etiCeMois = new Label("#########"+ConstantesCalendrier.Mois.values() [ceMois.getMois()-1]+ " " + ceMois.getAnnee()+"#########");
-        etiCeMois.setId("mois");
-        getChildren().add(etiCeMois);
+        StackPane monthStackPane = new StackPane();
 
-        VBox dates = new VBox();
-        dates.setSpacing(spacing);
-        for (DateCalendrier date : ceMois.getDates()) {
-            //System.out.println(date);
-            if ( date.getMois() == ceMois.getMois()) {
-                Label etiCetteDate = new Label(date.toString());
-                etiCetteDate.setId("jours");
-                dates.getChildren().add(etiCetteDate);
-                VBox.setMargin(etiCetteDate,new Insets(5,0,5,20));
+        Label etiDuMois = new Label(ConstantesCalendrier.Mois.values() [ceMois.getMois()-1]+ " " + ceMois.getAnnee());
+        etiDuMois.setId("mois");
+        VBox.setMargin(etiDuMois, new Insets(15));
+        getChildren().add(etiDuMois);
+        for (int i =1; i <= 12 ; i++){
+
+
+            CalendrierDuMois idxMois = new CalendrierDuMois(i,dateDuJour.getAnnee());
+
+
+
+            VBox dates = new VBox();
+            dates.setSpacing(spacing);
+            for (DateCalendrier date : idxMois.getDates()) {
+                //System.out.println(date);
+                if (date.getMois() == idxMois.getMois()) {
+                    Label etiCetteDate = new Label(date.toString());
+                    etiCetteDate.setId("jours");
+                    dates.getChildren().add(etiCetteDate);
+                    VBox.setMargin(etiCetteDate, new Insets(0, 0, 5, 20));
+                }
+
             }
+
+            ScrollPane scrollPaneDates = new ScrollPane();
+            scrollPaneDates.setContent(dates);
+            monthStackPane.getChildren().add(scrollPaneDates);
+
         }
-        ScrollPane scrollPaneDates = new ScrollPane();
-        scrollPaneDates.setContent(dates);
-        super.getChildren().add(scrollPaneDates);
+
+        super.getChildren().add(monthStackPane);
+
+
 
     }
 
