d9247339ae59e70ca92ff9309311901f6ad2d851
Alexis Morel <22501205@me.uvsq.fr>
2026-04-09 11:38:24 +0200

    création des tilepane et étiquettes pour chaque jour

--- src/main/java/vue/VboxRootCalendrier.java
+++ src/main/java/vue/VboxRootCalendrier.java
@@ -1,7 +1,35 @@
 package vue;
 
+import javafx.scene.control.Label;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.TilePane;
 import modele.*;
 
+import java.util.Calendar;
+
 public class VboxRootCalendrier {
-    Date today = new DateCalendrier();
+    public VboxRootCalendrier() {
+        Date today = new DateCalendrier();
+
+        StackPane monthStackPane = new StackPane();
+
+        ToggleGroup buttonGroup = new ToggleGroup();
+        for (int idxMois = 1; idxMois <= 12 ; idxMois ++ ){
+            CalendrierDuMois moisCalendrier = new CalendrierDuMois(idxMois, today.getAnnee());
+            TilePane tilePaneMois = new TilePane();
+
+            tilePaneMois.setPrefColumns(7);
+
+            tilePaneMois.setPrefRows(moisCalendrier.getDates().size() /7 + 1);
+
+            tilePaneMois.setId("opaque");
+
+            for (ConstantesCalendrier.Jours jour : ConstantesCalendrier.Jours.values()){
+                Label jourLabel = new Label(jour.toString().substring(0,2));
+                tilePaneMois.getChildren().add(jourLabel);
+            }
+        }
+    }
+
 }
