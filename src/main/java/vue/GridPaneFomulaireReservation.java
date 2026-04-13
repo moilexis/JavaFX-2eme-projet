d601ad56e7103943d4257c2a44fe5042646992a0
Alexis Morel <22501205@me.uvsq.fr>
2026-04-13 13:43:36 +0200

    Fin du TP4 et ajout des paramètres d'accessibilité

--- src/main/java/vue/GridPaneFomulaireReservation.java
+++ src/main/java/vue/GridPaneFomulaireReservation.java
@@ -26,7 +26,7 @@ public class GridPaneFomulaireReservation extends GridPane {
         add(lblCours, 1, 1, 1, 1);
         TextField tfCours = new TextField();
         lblCours.setLabelFor(tfCours);
-        add(tfCours, 2, 1, 3, 1);
+        add(tfCours, 2, 1, 4, 1);
 
 
         //NIVEAU DU COURS
@@ -52,27 +52,54 @@ public class GridPaneFomulaireReservation extends GridPane {
         add(btnExp, 4, 3, 2, 1);
 
         //HORAIRES
+        String[] HEURES = {"0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","23","24"};
+        String[] MINUTES = {"0","15","30","45"};
         Label Horaire =  new Label("horaire");
         add(Horaire, 1, 4, 1, 1);
         Label hor1 = new Label("de");
         add(hor1, 2, 4, 1, 1);
+
+        ComboBox<String> cbHeure1 = peupleComboBox(HEURES);
+        cbHeure1.getSelectionModel().select(7);
+        add(cbHeure1,3, 4, 1, 1);
         Label heure1 = new Label("h");
         add(heure1, 4, 4, 1, 1);
+
+        ComboBox<String> cbMin1 = peupleComboBox(MINUTES);
+        cbMin1.getSelectionModel().select(0);
+        add(cbMin1, 5, 4, 1, 1);
         Label min1 = new Label("min");
         add(min1, 6, 4, 1, 1);
 
         Label hor2 = new Label("à");
         add(hor2, 2,5 , 1, 1);
+        ComboBox<String> cbHeure2 = peupleComboBox(HEURES);
+        cbHeure2.getSelectionModel().select(8);
+        add(cbHeure2, 3, 5, 1, 1);
         Label heure2 = new Label("h");
         add(heure2, 4,5, 1, 1);
+
+        ComboBox<String> cbMin2 = peupleComboBox(MINUTES);
+        cbMin2.getSelectionModel().select(0);
+        add(cbMin2, 5, 5, 1, 1);
         Label min2 = new Label("min");
         add(min2, 6, 5, 1, 1);
 
         //VALIDATION
         Button annuler = new Button("_Annuler");
+        annuler.setMnemonicParsing(true);
         add(annuler, 3, 6, 2, 1);
-        Button enregistrer = new Button("Enregistrer");
+        Button enregistrer = new Button("_Enregistrer");
+        enregistrer.setMnemonicParsing(true);
         add(enregistrer, 5, 6, 2, 1);
 
+
+    }
+    private ComboBox<String> peupleComboBox(String[] strings){
+        ComboBox<String> cb = new ComboBox<>();
+        for (String s : strings){
+            cb.getItems().add(s);
+        }
+        return cb;
     }
 }
